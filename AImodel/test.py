ed8677edcc08911ea3d9f5f1d5ad7e6d90f82cc0
Faiz Hussain <129678701+princeiit423@users.noreply.github.com>
2025-04-09 20:52:50 +0530

    Delete AImodel directory

--- AImodel/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:8000/predict"  # Change endpoint if needed
-files = {"file": open("demo.jpeg", "rb")}
-
-response = requests.post(url, files=files)
-print(response.json())  # Print prediction result
